da18a954b1ad279b5c8e62f4f57fcf3cad4f9268
Ben Pearlstine <ben.pearlstine@gmail.com>
2020-07-07 18:33:21 -0400

    who would have thought there are so many ways to say hello world

--- basics.rb
+++ basics.rb
@@ -3,3 +3,14 @@ print "Hello World"
 # has a new line
 puts "Hello World" 
 p "Hello World"
+
+greeting = "Hello World, but a var"
+
+puts greeting
+
+def say_hello(thing_to_say)
+    puts thing_to_say
+    
+end
+
+say_hello "Hello World, but a function"
