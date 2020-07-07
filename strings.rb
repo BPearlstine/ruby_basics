038cb26650a69023c8fa7a555da1a4f41f4d4c9c
Ben Pearlstine <ben.pearlstine@gmail.com>
2020-07-07 18:54:37 -0400

    string basics

---
+++ strings.rb
@@ -0,0 +1,23 @@
+sentence = "what an incredible string"
+
+p sentence
+
+first_name = "Ben"
+last_name = "Pearlstine"
+#some common methods
+puts first_name.class
+puts first_name.length
+puts first_name.reverse
+puts first_name.capitalize
+puts first_name.empty?
+
+# concat
+puts first_name + " " + last_name
+
+# interpolation
+puts "My first name #{first_name} is and my last name is #{last_name}"
+
+#sub
+sentence = "welcome to my home"
+puts sentence.sub("home", "office")
+
